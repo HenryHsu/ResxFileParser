68b7c4e71104cd315fdc0b81a562423e4eb06cd0
henry.hsu <hsu7117@gmail.com>
2015-08-19 15:17:26 +0800

    First Release

---
+++ ResxFileParser/ResxFileParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ResxFileParser
+{
+    public class ResxFileParser
+    {
+        public Dictionary<string, string> DataName;
+        
+        private void FileParser(string FileName)
+        {
+            StreamReader ResxSR = new StreamReader(FileName);
+
+            Regex ResxPattern = new Regex("<data name=\"(?<DataName>.+?)\"\\sxml:space=\".+?\">[\\r\\n\\s]*<value>(?<Value>.+?)</value>[\\r\\n\\s]*</data>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            MatchCollection ResxDataCollection = ResxPattern.Matches(ResxSR.ReadToEnd());
+            foreach(Match SingleMatch in ResxDataCollection)
+            {
+                DataName.Add(SingleMatch.Groups["DataName"].Value, SingleMatch.Groups["Value"].Value);
+            }
+        }
+    }
+}
